67d52a92d33810f46a0a997942ed0a82fa0114d3
remya9946944014 <remyakochukunnel@gmail.com>
2020-12-04 20:04:22 +0530

    initialcheckin

---
+++ mvc/controllers/user_controller.py
@@ -0,0 +1,73 @@
+from flask import make_response, jsonify
+import mvc.model.mongoclient
+from werkzeug.security import generate_password_hash
+import logging
+
+
+class User:
+
+    @classmethod
+    def get_user_details(cls, search_condition, customer_identifier):
+        try:
+            if customer_identifier is None:
+                return make_response(jsonify({"message": "Invalid/Search criteria!"}), 400)
+            else:
+                data = mvc.model.mongoclient.MongoModel.get_record(search_condition, customer_identifier)
+                if len(data) > 0:
+                    return data
+                return make_response(jsonify({"message": "No records found!"}), 200)
+        except Exception as e:
+            raise e
+
+    @classmethod
+    def delete_user(cls, user_id):
+        return mvc.model.mongoclient.MongoModel.delete_record('user_id', user_id)
+
+    @classmethod
+    def add_user(cls, request_data):
+        _id = None
+        try:
+            request_data["user_id"] = mvc.model.mongoclient.MongoModel.get_document_number("user_id") + 1
+            request_data["password"] = generate_password_hash(request_data["password"])
+            _id = mvc.model.mongoclient.MongoModel.add_record(request_data)
+            if _id:
+                user_id = request_data["user_id"]
+                response = make_response(jsonify({"user_id": user_id}), 200)
+                response.headers["trace_id"] = _id
+            else:
+                response = make_response(jsonify({"Warning": "Customer creation failed."}), 500)
+        except Exception as ex:
+            response = make_response(jsonify({"Error": "Error occurred on Customer creation."}+ex), 500)
+        return response
+
+
+class Loan:
+    @classmethod
+    def add_loan(cls, request_data):
+        _id = None
+        try:
+            _id = mvc.model.mongoclient.MongoModel.add_record(request_data, "Loan")
+            if _id:
+                response = make_response(jsonify({"Success": "Loan added."}), 200)
+            else:
+                response = make_response(jsonify({"Warning": "Loan creation failed."}), 500)
+        except Exception as ex:
+            response = make_response(jsonify({"Error": "Error occurred on Loan creation."}+ex), 500)
+        return response
+
+    @classmethod
+    def get_loan_details(cls, loanid):
+        try:
+            if loanid is None:
+                return make_response(jsonify({"message": "Invalid/Search criteria!"}), 400)
+            else:
+                data = mvc.model.mongoclient.MongoModel.get_record('loanid', loanid, 'Loan')
+                if len(data) > 0:
+                    return data
+                return make_response(jsonify({"message": "No records found!"}), 200)
+        except Exception as e:
+            raise e
+
+    @classmethod
+    def delete_loan(cls, loanid):
+        return mvc.model.mongoclient.MongoModel.delete_record('loanid', loanid, 'Loan')
---
+++ mvc/model/mongoclient.py
@@ -0,0 +1,51 @@
+import pymongo
+from pymongo import MongoClient
+import logging
+
+
+class MongoModel:
+    mc = MongoClient("mongodb://localhost:27017")
+    db = mc["Bank"]
+    collection_name = db["Users"]
+
+    @classmethod
+    def get_document_number(cls, doc_id, collection_name=None):
+        try:
+            if collection_name:
+                cls.collection_name = cls.db[collection_name]
+            doc_id = cls.collection_name.find().sort(doc_id, pymongo.DESCENDING).limit(1)[0][doc_id]
+        except BaseException as ex:
+            doc_id = 0
+            logging.error(ex)
+        return doc_id
+
+    @classmethod
+    def get_record(cls, identifier, identifier_value=None, collection_name=None):
+        if collection_name:
+            cls.collection_name = cls.db[collection_name]
+        if identifier_value:
+            data = cls.collection_name.find({identifier: identifier_value}, {"_id": 0})
+        else:
+            data = cls.collection_name.find({}, {"_id": 0})
+        records = dict(data=[])
+        for item in data:
+            records["data"].append(item)
+        return records
+
+    @classmethod
+    def delete_record(cls, identifier, identifier_value=None, collection_name=None):
+        if collection_name:
+            cls.collection_name = cls.db[collection_name]
+        response = cls.collection_name.delete_one({identifier: identifier_value})
+        return str(response.deleted_count)
+
+    @classmethod
+    def add_record(cls, request_data, collection_name=None):
+        if collection_name:
+            cls.collection_name = cls.db[collection_name]
+        response = cls.collection_name.insert_one(request_data)
+        return response.inserted_id
+
+    @classmethod
+    def update_record(cls, request_data, collection_name=None):
+        pass
